d0e7decc0dd69608db0e4e9e80016c40156abe69
Ebraheem Mahmood <ebraheemmahmood991@gmail.com>
2020-06-05 09:38:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+
+
+
+
+
+
+
+   let persons =[]
+
+function writeCards(a, b){
+  
+  for(var i=0; i<a.length ; i++){
+ 
+    a.push('Thank you, '+a[i]+', for the wonderful '+b+' gift!');
+
+  }
+  // console.log(persons)
+  return a
+}
+
+
+
+writeCards(['Lisa', 'Kaitlin', 'Jan'], "surprise" );
+// function  countDown(a){
+// while(i!==0){
+//     i--
+//     console.log(i)
+//   }
+// }
+// console.log(countDown(10))
